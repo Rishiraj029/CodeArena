5866f02f9cc3318ab2175c0cfc696affacc7488d
Rishiraj Singh <rishirajsinghrathore029@gmail.com>
2026-04-27 17:16:07 +0530

    Resolve merge conflicts in backend files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,29 @@
 import express from "express";
 import path from "path";
 
+import cors from "cors";
+import {serve} from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
+
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { inngest, syncUser, deleteUserFromDB } from "./lib/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve()
 
+
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+app.use(clerkMiddleware())
+
+app.use("/api/inngest", serve({ client: inngest, functions: [syncUser, deleteUserFromDB] }))
+
+
 app.get("/health", (req, res) => {
+  req.auth
   res.status(200).json({msg:"api is up and running"})
 });
 
