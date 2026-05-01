1fd2918de3ccf51898b5c893d12944237e7f0264
Rishiraj Singh <rishirajsinghrathore029@gmail.com>
2026-05-01 15:03:39 +0530

    Add protectRoute middleware and package-lock.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,11 @@ import {serve} from "inngest/express";
 import { clerkMiddleware } from '@clerk/express'
 
 
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest, syncUser, deleteUserFromDB } from "./lib/inngest.js";
+import  chatRoutes  from "./routes/chatRoutes.js";
 
 const app = express();
 
@@ -16,19 +18,19 @@ const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
-app.use(clerkMiddleware())
+app.use(clerkMiddleware()); 
 
 app.use("/api/inngest", serve({ client: inngest, functions: [syncUser, deleteUserFromDB] }))
+app.use("/api/chat", chatRoutes)
 
 
 app.get("/health", (req, res) => {
-  req.auth
   res.status(200).json({msg:"api is up and running"})
 });
 
-app.get("/books", (req, res) => {
-  res.status(200).json({msg:"this is the books endpoint"})
-});
+
+
+
 
 
 
